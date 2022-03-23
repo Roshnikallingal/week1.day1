9bda056d4148ba3e6c34732c8b90c6df1e2b99dc
Roshnikallingal <102172183+Roshnikallingal@users.noreply.github.com>
2022-03-23 13:20:14 +0530

    Add files via upload

---
+++ factorial.java
@@ -0,0 +1,16 @@
+package week1.day1.classroom;
+
+public class factorial {
+	
+	public static void main(String[] args) {
+		int i,fact =1,num=5;
+		for(i=1;i<=num;i++)
+		{
+			fact=fact*i;
+			
+		}
+		System.out.println(fact);
+		
+	}
+
+}
